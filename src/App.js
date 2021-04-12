9f739cdba73d86cbf0c9e32e81c06cfd1f6a699b
Ryan <ryan.j.z@hotmail.com>
2021-04-12 06:22:49 +0200

    form test

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Form from "./Form"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form name="contact" method="POST">
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out if you’re human:
-              <input name="bot-field" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Dog’s Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              File: <input type="file" name="test-file" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <Form />
       </header>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import React from "react";
+import { navigateTo } from "gatsby-link";
+
+function encode(data) {
+  const formData = new FormData();
+
+  for (const key of Object.keys(data)) {
+    formData.append(key, data[key]);
+  }
+
+  return formData;
+}
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleAttachment = (e) => {
+    this.setState({ [e.target.name]: e.target.files[0] });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
+
+  render() {
+    return (
+      <>
+        <form name="contact" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out if you’re human:
+              <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Dog’s Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              File: <input type="file" name="test-file" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </>
+    );
+  }
+}
+
