ad57381178bb1ae4c8e83642df8b9bd29ed0d294
Ryan <ryan.j.z@hotmail.com>
2021-04-12 05:57:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form name="contact" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out if you’re human:
+              <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Dog’s Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              File: <input type="file" name="test-file" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </header>
     </div>
   );
